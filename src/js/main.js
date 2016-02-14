21b9e254f42e78c332cb5f01449db38677d9ec04
Xavier Reyes <xareyesochoa@gmail.com>
2016-02-13 16:45:01 -0800

    Added toggle navigation button using JQuery

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,27 @@
-(function(){
+(function() {
 
-  console.log('TODO: Add nav interactivity');
+    var toggleButton = $('.toggle-btn');
+
+    function showToggleButton() {
+        if ($(window).width() < 768) {
+            toggleButton.css('display', 'block');
+        } else {
+            toggleButton.css('display', 'none');
+        }
+    }
+
+    //on load
+    showToggleButton();
+    
+    //on resize
+    $(window).resize(function() {
+        showToggleButton();
+    });
+
+    //on click
+    toggleButton.click(function() {
+        $('.main-nav').toggleClass('open');
+    })
+
 
 })();
